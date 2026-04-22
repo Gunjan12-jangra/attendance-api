844599d2e9398b06870cf27c44114a3716f45edd
Ubuntu <ubuntu@ip-172-31-0-54.ap-south-1.compute.internal>
2026-04-22 16:02:37 +0000

    Update config, tests and liquibase setup

--- client/tests/test_redis_conn.py
+++ client/tests/test_redis_conn.py
@@ -3,7 +3,8 @@ from unittest import mock
 from client.redis import MiddlewareSDKFacade
 from client.redis.redis_conn import get_caching_data
 
-@mock.patch('client.redis.open')
+
+@mock.patch('builtins.open')
 def test_get_caching_data(mock_open):
     mock_file = mock.MagicMock()
     mock_file.__enter__.return_value = mock_file
@@ -21,18 +22,15 @@ def test_get_caching_data(mock_open):
 
     assert result == {
         "CACHE_TYPE": "redis",
-        "CACHE_REDIS_HOST": "172.17.0.4",
+        "CACHE_REDIS_HOST": "localhost",
         "CACHE_REDIS_PORT": 6379,
-        "CACHE_REDIS_URL": "redis://172.17.0.4:6379/0"
+        "CACHE_REDIS_URL": "redis://localhost:6379/0"
     }
 
-    # mock_open.assert_called_once_with('config.yaml', 'r', encoding="utf-8")
-    # mock_file.read.assert_called_once()
-    #
-    # del os.environ['CONFIG_FILE']
+    del os.environ['CONFIG_FILE']
 
 
-@mock.patch('client.redis.open')
+@mock.patch('builtins.open')
 def test_redis_status(mock_open):
     mock_file = mock.MagicMock()
     mock_file.__enter__.return_value = mock_file
@@ -47,12 +45,7 @@ def test_redis_status(mock_open):
     os.environ['CONFIG_FILE'] = 'config.yaml'
 
     redis_client = MiddlewareSDKFacade.cache.redis_status()
-    # mock_redis.assert_called_once_with(
-    #     host="localhost",
-    #     port=6379,
-    #     password="mypassword",
-    #     decode_responses=True
-    # )
+
     assert redis_client == "up"
 
     del os.environ['CONFIG_FILE']
